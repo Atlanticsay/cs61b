9b1c915b45492e4324065c4b32a29eb1fcbc6db7
Atlantic <2274977981@qq.com>
2020-03-02 23:03:08 +0800

    finish hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,148 @@
+package synthesizer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+//TODO: Make sure to make this class and all of its methods public
+//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
+public class ArrayRingBuffer<T> implements BoundedQueue<T> {
+    /* First stores the index of the least recently inserted item. */
+    private int first;
+    /* Last stores the index one beyond the most recently inserted item. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+    /* Non-empty elments in the array.*/
+    private int fillCount;
+    /* Capacity of the array. */
+    int capacity;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // TODO: Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+        //       this.capacity should be set appropriately. Note that the local variable
+        //       here shadows the field we inherit from AbstractBoundedQueue, so
+        //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+    }
+
+    /** Return size of the buffer */
+    public int capacity() {
+        return this.capacity;
+    }
+
+    /** return number of items currently in the buffer. */
+    public int fillCount() {
+        return this.fillCount;
+    }
+
+    /** return the pointer next to the given one. */
+    public int nextPointerInArrayRing(int pos) {
+        if (pos == (capacity - 1)) {
+            pos = 0;
+            return pos;
+        }
+        pos += 1;
+        return pos;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (fillCount == capacity) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last = nextPointerInArrayRing(last);
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring Buffer underflow");
+        }
+        T oldFirst = rb[first];
+        rb[first] = null;
+        first = nextPointerInArrayRing(first);
+        fillCount--;
+        return oldFirst;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     * Return (but do not delete) item from the front
+     */
+    public T peek() {
+        // TODO: Return the first item. None of your instance variables should change.
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring Buffer underflow");
+        }
+        return rb[first];
+    }
+
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+
+
+    @Override
+    public boolean equals(Object o) {
+        ArrayRingBuffer<T> obj = (ArrayRingBuffer<T>) o;
+        int p = first;
+        for (T t : obj) {
+            t = (T) t;
+            if (t != rb[p]) {
+                return false;
+            }
+            p = nextPointerInArrayRing(p);
+        }
+        return true;
+    }
+
+    /** returns an iterator */
+    public Iterator<T> iterator() {
+        return new ArrayRingIterator();
+    }
+
+    private class ArrayRingIterator implements Iterator<T> {
+        private int wizPos;
+        public ArrayRingIterator() {
+            wizPos = first;
+        }
+
+        /*  Returns true if the iteration has more elements. */
+        public boolean hasNext() {
+            if ((nextPointerInArrayRing(wizPos) == last) || (rb[wizPos] == null)) {
+                return false;
+            }
+            return true;
+        }
+
+        /* Mover the pointer wizPos to the next.
+         * @returns the next value in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements */
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T returnItem = rb[wizPos];
+            wizPos = nextPointerInArrayRing(wizPos);
+            return returnItem;
+        }
+    }
+
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,19 +1,88 @@
 package synthesizer;
 import org.junit.Test;
+
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+ *  @author Atlantic
  */
 
 public class TestArrayRingBuffer {
+
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void basicTests() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        assertTrue(arb.isEmpty());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertFalse(arb.isFull());
+        assertEquals(1, arb.dequeue());
+        assertEquals(2, arb.peek());
+        arb.enqueue(6);
+        arb.enqueue(4);
+        assertTrue(arb.isFull());
     }
 
-    /** Calls tests for ArrayRingBuffer. */
-    public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+    @Test
+    public void iteratorTests() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(5);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+
+        Iterator<Integer> arbIterator = arb.iterator();
+        int actual = arbIterator.next();
+        assertEquals(1, actual);
+
+        arbIterator.next();
+        assertTrue(arbIterator.hasNext());
+        arbIterator.next();
+        assertFalse(arbIterator.hasNext());
+
+        ArrayRingBuffer arb2 = new ArrayRingBuffer(5);
+        arb2.enqueue(1);
+        for (Object element : arb2) {
+            element = (int) element;
+            assertEquals(1, element);
+        }
+
     }
-} 
+
+
+    @Test
+    public void equalTests() {
+
+        ArrayRingBuffer arb = new ArrayRingBuffer(5);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+
+        ArrayRingBuffer arb2 = new ArrayRingBuffer(5);
+        arb2.enqueue(1);
+        arb2.enqueue(2);
+        arb2.enqueue(3);
+        arb2.enqueue(4);
+
+        ArrayRingBuffer arb3 = new ArrayRingBuffer(5);
+        arb3.enqueue(1);
+        arb3.enqueue(8);
+        arb3.enqueue(3);
+        arb3.enqueue(6);
+
+        assertTrue(arb.equals(arb2));
+        assertFalse(arb.equals(arb3));
+
+
+    }
+
+//    /** Calls tests for ArrayRingBuffer. */
+//    public static void main(String[] args) {
+//        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+//    }
+
+}
